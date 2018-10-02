bdd76b63923325661b8a91b4b3b9b6e888eb6e70
graham-willis-druid <graham.willis.ea@gmail.com>
2018-10-02 12:47:08 +0100

    Add enhanced decorator to the autorunner

--- test/auto-runner/runner.js
+++ test/auto-runner/runner.js
@@ -129,13 +129,29 @@ const internals = {
     server.decorate('request', 'cache', function () {
       return {
         get: async () => {
-          return this.server.app.cache.get(this.auth.artifacts.sid)
+          try {
+            const result = await this.server.app.cache.get(this.auth.artifacts.sid)
+            logger.debug(`cache read: ${this.auth.artifacts.sid}: ${JSON.stringify(result)}`)
+            return result
+          } catch (err) {
+            throw new Error('Cache fetch error')
+          }
         },
         set: async (obj) => {
-          await this.server.app.cache.set(this.auth.artifacts.sid, obj)
+          try {
+            logger.debug(`cache write: ${this.auth.artifacts.sid}: ${JSON.stringify(obj)}`)
+            await this.server.app.cache.set(this.auth.artifacts.sid, obj)
+          } catch (err) {
+            throw new Error('Cache put error')
+          }
         },
         drop: async () => {
-          await this.server.app.cache.drop(this.auth.artifacts.sid)
+          try {
+            await this.server.app.cache.drop(this.auth.artifacts.sid)
+            logger.debug(`cache drop: ${this.auth.artifacts.sid}`)
+          } catch (err) {
+            throw new Error('Cache drop error')
+          }
         }
       }
     })
