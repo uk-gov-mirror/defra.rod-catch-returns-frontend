1e1b9ef213f94c57a5847a5a18ad6c88c425ec83
graham-willis-druid <graham.willis.ea@gmail.com>
2018-10-02 16:18:02 +0100

    Bring in the large catch

--- test/auto-runner/auto-run.js
+++ test/auto-runner/auto-run.js
@@ -46,11 +46,11 @@ experiment('Scripted regression tests', () => {
   test('Small catch', async () => {
     await Runner.run(require('./scripts/small-catch'))
   })
-  //
-  // test('Large catch', async () => {
-  //   await Runner.run(require('./scripts/large-catch'))
-  // })
-  //
+
+  test('Large catch', async () => {
+    await Runner.run(require('./scripts/large-catch'))
+  })
+
   // test('Review and submit', async () => {
   //   await Runner.run(require('./scripts/review-and-submit'))
   // })
