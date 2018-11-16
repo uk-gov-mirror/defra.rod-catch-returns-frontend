6b8dbe94f68a9636764b0bbbd04803795cdbdb57
graham-willis-druid <graham.willis.ea@gmail.com>
2018-11-16 14:51:20 +0000

    Tweak global cookie message

--- src/validators/login.js
+++ src/validators/login.js
@@ -3,10 +3,7 @@
 /**
  * Validate the user login
  */
-const { logger } = require('defra-logging-facade')
-
 module.exports = async (request) => {
-  logger.debug('Validate login: ' + JSON.stringify(request.payload.user))
   if (request.app.authorization) {
     return null
   } else {
