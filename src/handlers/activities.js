uk-gov-mirror/defra.rod-catch-returns-frontend

commit 66416f4ad9d2824b362411b1fc3a0d6896bac950

Avoid the /reports?back=/summary pattern due to danger of reflected xss

src/handlers/activities.js
@@ -41,7 +41,7 @@ module.exports = class ActivitiesHandler extends BaseHandler {
     }
 
     if (request.params.id === 'add') {
-      delete cache.activity
+      cache.back = request.path
       await request.cache().set(cache)
 
       // Filter out the rivers already selected
@@ -71,6 +71,7 @@ module.exports = class ActivitiesHandler extends BaseHandler {
 
       // Write the catch id onto the cache
       cache.activity = { id: activity.id }
+      cache.back = request.path
       await request.cache().set(cache)
 
       // Prepare a the payload

src/handlers/delete-activity.js
@@ -56,6 +56,7 @@ module.exports = class DeleteActivityHandler extends BaseHandler {
     }
 
     cache.delete = activity.id
+    cache.back = request.path
     await request.cache().set(cache)
     return h.view(this.path, { river,
       details: {

src/handlers/delete-salmon-and-large-trout.js
@@ -58,6 +58,7 @@ module.exports = class DeleteRiverHandler extends BaseHandler {
 
     // Save the id to delete
     cache.delete = largeCatch.id
+    cache.back = request.path
     await request.cache().set(cache)
     return h.view(this.path, { largeCatch: c,
       details: {

src/handlers/delete-small-catch.js
@@ -67,6 +67,7 @@ module.exports = class DeleteRiverHandler extends BaseHandler {
 
     // Save the id to delete
     cache.delete = smallCatch.id
+    cache.back = request.path
     await request.cache().set(cache)
     return h.view(this.path, { smallCatch: c,
       details: {

src/handlers/did-you-fish.js
@@ -47,6 +47,7 @@ module.exports = class DidYouFishHandler extends BaseHandler {
 
     // Set the submissionId in the cache
     cache.submissionId = submission.id
+    cache.back = request.path
     await request.cache().set(cache)
 
     // If we have any activity go straight to the summary screen

src/handlers/reports.js
@@ -18,6 +18,6 @@ module.exports = class ReportsHandler extends BaseHandler {
    * @returns {Promise<*>}
    */
   async doGet (request, h) {
-    return this.readCacheAndDisplayView(request, h, { back: request.query.back })
+    return this.readCacheAndDisplayView(request, h)
   }
 }

src/handlers/review.js
@@ -44,6 +44,9 @@ module.exports = class ReviewHandler extends BaseHandler {
 
   async reviewReturn (request, h) {
     const cache = await request.cache().get()
+    cache.back = request.path
+    await request.cache().set(cache)
+
     let submission = await submissionsApi.getById(request, cache.submissionId)
 
     // Get the activities

src/handlers/salmon-and-large-trout.js
@@ -40,6 +40,7 @@ module.exports = class SalmonAndLargeTroutHandler extends BaseHandler {
     }
 
     const cache = await request.cache().get()
+    cache.back = request.path
     const submission = await submissionsApi.getById(request, cache.submissionId)
     const activities = await activitiesApi.getFromLink(request, submission._links.activities.href)
     let rivers = activities.map(a => a.river)

commit 6725bc6272b3ecbd2c76d6dbbf8e17046e2e0432

Fish error introduced in activity handler in 66416f4ad9d2824b362411b1fc3a0d6896bac950

src/handlers/activities.js
@@ -41,6 +41,7 @@ module.exports = class ActivitiesHandler extends BaseHandler {
     }
 
     if (request.params.id === 'add') {
+      delete cache.activity
       cache.back = request.path
       await request.cache().set(cache)
 