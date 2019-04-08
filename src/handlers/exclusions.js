67c4886d5441162dae7cdda9e10912c12a3906af
graham-willis-druid <graham.willis.ea@gmail.com>
2019-04-08 15:32:26 +0100

    Work in progress - exclusion flags

--- src/handlers/exclusions.js
+++ src/handlers/exclusions.js
@@ -1,4 +1,11 @@
 'use strict'
+const SubmissionsApi = require('../api/submissions')
+const CatchesApi = require('../api/catches')
+const SmallCatchesApi = require('../api/small-catches')
+
+const submissionsApi = new SubmissionsApi()
+const catchesApi = new CatchesApi()
+const smallCatchesApi = new SmallCatchesApi()
 
 /**
  * Write the state of the exclusions flags into the cache
@@ -18,7 +25,50 @@ module.exports = class ExclusionsHandler extends BaseHandler {
    * @returns {Promise<*>}
    */
   async doPost (request, h) {
-    console.log(JSON.stringify(request.payload))
+    const cache = await request.cache().get()
+    const submission = await submissionsApi.getById(request, cache.submissionId)
+    const catches = await catchesApi.getFromLink(request, submission._links.catches.href)
+    const smallCatches = await smallCatchesApi.getFromLink(request, submission._links.smallCatches.href)
+
+    const payloadKey = Object.keys(request.payload)[0]
+
+    if (payloadKey === 'exclude-1') {
+      const setting = request.payload[payloadKey]
+
+      // Change the submission level exclusion if necessary
+      if (submission.reportingExclude !== setting) {
+        await submissionsApi.changeExclusion(request, submission.id, setting)
+      }
+
+      // Cascade the small catch exclusions if necessary
+      await Promise.all(smallCatches.map(async c => {
+        if (c.reportingExclude !== setting) {
+          await smallCatchesApi.changeExclusion(request, c.id, setting)
+        }
+      }))
+
+      // Cascade the large catch exclusions if necessary
+      await Promise.all(catches.map(async c => {
+        if (c.reportingExclude !== setting) {
+          await catchesApi.changeExclusion(request, c.id, setting)
+        }
+      }))
+    } else if (payloadKey.includes('smallCatches')) {
+      const key = payloadKey.replace('exclude-', '')
+      const setting = request.payload[payloadKey]
+      const smallCatch = smallCatches.find(c => c.id === key)
+      if (smallCatch && smallCatch.reportingExclude !== setting) {
+        await smallCatchesApi.changeExclusion(request, key, setting)
+      }
+    } else if (payloadKey.includes('catches')) {
+      const key = payloadKey.replace('exclude-', '')
+      const setting = request.payload[payloadKey]
+      const largeCatch = catches.find(c => c.id === key)
+      if (largeCatch && largeCatch.reportingExclude !== setting) {
+        await catchesApi.changeExclusion(request, key, setting)
+      }
+    }
+
     return null
   }
 }
--- src/handlers/summary.js
+++ src/handlers/summary.js
@@ -72,67 +72,11 @@ module.exports = class SummaryHandler extends BaseHandler {
    */
   async doPost (request, h) {
     if (process.env.CONTEXT === 'FMT') {
-      await this.exclusions(request)
-
       if (Object.keys(request.payload).includes('save')) {
         return h.redirect('/save')
       }
     }
 
     return h.redirect('/review')
-  }
-
-  async exclusions (request) {
-    const cache = await request.cache().get()
-    const submission = await submissionsApi.getById(request, cache.submissionId)
-
-    // Process the payload
-    const smallCatchExclusions = request.payload['exclude-small-catch']
-      ? request.payload['exclude-small-catch'].split(',')
-      : null
-    const catchExclusions = request.payload['exclude-catch']
-      ? request.payload['exclude-catch'].split(',')
-      : null
-
-    // Get the small and large catches
-    const catches = await catchesApi.getFromLink(request, submission._links.catches.href)
-    const smallCatches = await smallCatchesApi.getFromLink(request, submission._links.smallCatches.href)
-
-    // Change the submission level exclusion if necessary
-    if (Object.keys(request.payload).includes('exclude')) {
-      if (!submission.reportingExclude) {
-        await submissionsApi.changeExclusion(request, submission.id, true)
-      }
-    } else {
-      if (submission.reportingExclude) {
-        await submissionsApi.changeExclusion(request, submission.id, false)
-      }
-    }
-
-    // Change the large catch exclusions where necessary
-    await Promise.all(catches.map(async c => {
-      if (catchExclusions && catchExclusions.find(e => e === c.id)) {
-        if (!c.reportingExclude) {
-          await catchesApi.changeExclusion(request, c.id, true)
-        }
-      } else {
-        if (c.reportingExclude) {
-          await catchesApi.changeExclusion(request, c.id, false)
-        }
-      }
-    }))
-
-    // Change the small catch exclusions where necessary
-    await Promise.all(smallCatches.map(async c => {
-      if (smallCatchExclusions && smallCatchExclusions.find(e => e === c.id)) {
-        if (!c.reportingExclude) {
-          await smallCatchesApi.changeExclusion(request, c.id, true)
-        }
-      } else {
-        if (c.reportingExclude) {
-          await smallCatchesApi.changeExclusion(request, c.id, false)
-        }
-      }
-    }))
   }
 }
