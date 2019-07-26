80e8c51493b8001c4dce6cf8a79c2a80ce908f5c
alittlecross <alittlecross@alittlecross-MacBook-15.local>
2019-07-26 11:50:09 +0100

    conflict route, view, and handler added to deal with keys that have already been uploaded

---
+++ src/handlers/age-weight-key-conflict-check.js
@@ -0,0 +1,53 @@
+'use strict'
+const { logger } = require('defra-logging-facade')
+const Fs = require('fs')
+/**
+ * Display the age weight key conflict check
+ */
+const BaseHandler = require('./base')
+const AgeWeightKeyApi = require('../api/age-weight-key')
+
+module.exports = class AgeWeightKeyConflictCheck extends BaseHandler {
+  constructor (...args) {
+    super(args)
+  }
+
+  removeTempFile (filepath) {
+    Fs.unlinkSync(filepath)
+    logger.debug(`Removed temporary file: ${filepath}`)
+  }
+
+  /**
+   * Get handler for age weight key error breakdown
+   * @param request
+   * @param h
+   * @param user
+   * @returns {Promise<*>}
+   */
+  async doGet (request, h) {
+    const cache = await request.cache().get()
+    const key = cache.ageWeightKey.key ? cache.ageWeightKey.key : ''
+    const year = cache.ageWeightKey.year
+
+    return this.readCacheAndDisplayView(request, h, { key, year })
+  }
+
+  async doPost (request, h, errors) {
+    const overwrite = request.payload.overwrite
+    if (overwrite === 'true') {
+      const cache = await request.cache().get()
+      const filepath = cache.payload.upload.path
+      const year = cache.payload.year
+
+      await AgeWeightKeyApi.postNew(request, year, filepath, true)
+
+      this.removeTempFile(filepath)
+
+      return AgeWeightKeyConflictCheck.writeCacheAndRedirect(request, h, false, '/age-weight-key-ok', '')
+    } else if (overwrite === 'false') {
+      return AgeWeightKeyConflictCheck.writeCacheAndRedirect(request, h, false, '/age-weight-key', '')
+    } else {
+      return h.view(this.path, { error: true })
+    }
+  }
+}
