19b97a1c86daaa838ab5c842e76dec35ccca93c0
graham-willis-druid <graham.willis.ea@gmail.com>
2019-09-16 15:03:56 +0100

    Add debug. File upload tests don't work in travis.

--- test/auto-runner/uploads.js
+++ test/auto-runner/uploads.js
@@ -73,6 +73,7 @@ experiment('File upload: ', () => {
 
   test('No gate selected', async () => {
     const response = await makeUpload(YEAR, '', VALID_FILE)
+    if (response.statusCode !== 302) console.error({response})
     expect(response.statusCode).to.equal(302)
     expect(response.headers.location).to.equal('/age-weight-key')
   })
