55e9b2d913d225f55b97fa5e6aa64aba6d65d610
graham-willis-druid <graham.willis.ea@gmail.com>
2019-11-19 15:24:18 +0000

    Added script to control clam deamon
    request-checks: true

--- start-clam.sh
+++ start-clam.sh
@@ -1,11 +1,18 @@
 #!/usr/bin/env bash
 
-command -v systemctl || exit 0
-systemctl status clamav-freshclam || systemctl restart clamav-freshclam
+command -v systemctl || exit -1
+systemctl status clamav-freshclam || systemctl start clamav-freshclam
+x=1
 while [ ! -f /var/lib/clamav/daily.cld ]
 do
-    echo ...
+    echo -n .
     sleep 1
+    ((x++))
+    if [ $x -eq 120 ]
+    then
+        echo Cannot start clamav-daemon
+        exit -1
+    fi
 done
 sleep 5
-systemctl status clamav-daemon || systemctl restart clamav-daemon
+systemctl status clamav-daemon || systemctl start clamav-daemon
