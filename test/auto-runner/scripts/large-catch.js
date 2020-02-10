f44572f64729a55bdb307b3b7539454bfe7dc2f0
graham-willis-druid <graham.willis.ea@gmail.com>
2020-02-10 12:19:12 +0000

    Tray and up coverage

--- test/auto-runner/scripts/large-catch.js
+++ test/auto-runner/scripts/large-catch.js
@@ -10,7 +10,12 @@ module.exports = signIn.subsequent
   .concat(requests.addLargeCatch4)
   .concat(requests.editLargeCatch1)
   .concat(requests.editLargeCatch2)
+  .concat(requests.editLargeCatch3WithErrors)
+  .concat(requests.editLargeCatch4WithErrors)
   .concat(requests.deleteLargeCatch1)
   .concat(requests.deleteLargeCatch2)
+  .concat(requests.deleteLargeCatch3)
+  .concat(requests.deleteLargeCatch4)
   .concat(requests.addLargeCatchWithErrors1)
   .concat(requests.addLargeCatchWithErrors2)
+
