3dbe22c1010d8b6b1c86eaeb67f499b86e751d70
graham-willis-druid <graham.willis.ea@gmail.com>
2020-02-10 09:56:10 +0000

    Improve coverage

--- test/auto-runner/scripts/large-catch.js
+++ test/auto-runner/scripts/large-catch.js
@@ -6,6 +6,8 @@ const signIn = require('./sign-in')
 module.exports = signIn.subsequent
   .concat(requests.addLargeCatch1)
   .concat(requests.addLargeCatch2)
+  .concat(requests.addLargeCatch3)
+  .concat(requests.addLargeCatch4)
   .concat(requests.editLargeCatch1)
   .concat(requests.editLargeCatch2)
   .concat(requests.deleteLargeCatch1)
