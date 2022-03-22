6a35967cd9eee74f28dc533dd08b3d566e9a7d12
michael steinacher <steinacherm@hotmail.co.uk>
2022-03-22 13:22:42 +0000

    Added tests for activities constructor and amended the functions to use mocks correctly

--- test-jest/api/activities.test.js
+++ test-jest/api/activities.test.js
@@ -1,15 +1,24 @@
 const mockGetFromLink = jest.fn()
+const mockRequest = jest.fn()
 const ActivitiesApi = require('../../src/api/activities')
 const EntityApi = require('../../src/api/entity-api')
-const RiversApi = require('../../src/api/rivers')
+const Client = require('../../src/api/client')
 
-
+// jest.mock('../../src/api/entity-api')
 jest.mock('../../src/api/rivers', () => {
   return jest.fn().mockImplementation(() => {
     return {
       getFromLink: mockGetFromLink
     }
   })
+})
+
+jest.mock('../../src/api/client', () => {
+  const originalModule = jest.requireActual('../../src/api/client')
+  return {
+    ...originalModule,
+    request: mockRequest
+  }
 })
 
 describe('activities', () => {
@@ -29,48 +38,166 @@ describe('activities', () => {
       process.env.API_PATH = apiPath
       const activitiesApi = new ActivitiesApi()
       mockGetFromLink.mockImplementationOnce(() => ({ _links: { self: { href: href } } }))
-      const mapped = await activitiesApi.doMap(createMockContext({ href }), createMockRiver({ href }))
-      expect(mapped.id).toBe(expectedValue)
+      const mapped = await activitiesApi.doMap(({ }), createMockContext({ href }))
+      expect(mapped.id).toEqual(expectedValue)
+    })
+
+    it.each([
+      [1, 1],
+      [10, 10],
+      [0, 0]
+    ])('sets days fished with no release', async (daysFished, expectedValue) => {
+      const activitiesApi = new ActivitiesApi()
+      mockGetFromLink.mockImplementationOnce(() => ({ _links: { self: { href: 'http://example.com/path/to/somewhere' } } }))
+      const mapped = await activitiesApi.doMap(({ }), createMockContext({ href: 'http://example.com/path/to/somewhere', daysFishedOther: daysFished }))
+      expect(mapped.daysFishedOther).toEqual(expectedValue)
+    })
+
+    it.each([
+      [1, 1],
+      [10, 10],
+      [0, 0]
+    ])('sets days fished with mand release', async (daysFishedMand, expectedValue) => {
+      const activitiesApi = new ActivitiesApi()
+      mockGetFromLink.mockImplementationOnce(() => ({ _links: { self: { href: 'http://example.com/path/to/somewhere' } } }))
+      const mapped = await activitiesApi.doMap(({ }), createMockContext({ href: 'http://example.com/path/to/somewhere', daysFishedWithMandatoryRelease: daysFishedMand }))
+      expect(mapped.daysFishedWithMandatoryRelease).toEqual(expectedValue)
+    })
+
+    it.each([
+      ['http://example.com/path/to/somewhere', '/path', 'to/somewhere'],
+      ['http://example.com/unwise/path/to/the/dark/side', 'the', '/unwise/path/to/dark/side'],
+      ['http://example.com/unwise/path/to/the/dark/side/', 'side', '/unwise/path/to/the/dark/']
+    ])('sets river id to correct value', async (href, apiPath, expectedValue) => {
+      process.env.API_PATH = apiPath
+      const activitiesApi = new ActivitiesApi()
+      mockGetFromLink.mockImplementationOnce(() => ({ _links: { self: { href: href } } }))
+      const mapped = await activitiesApi.doMap(({ }), createMockContext({ href }))
+      expect(mapped.river.id).toEqual(expectedValue)
+    })
+
+    it.each([
+      ['Tyne', 'Tyne'],
+      ['Barry', 'Barry'],
+      ['Usk', 'Usk']
+    ])('sets river name value', async (riverName, expectedValue) => {
+      const activitiesApi = new ActivitiesApi()
+      mockGetFromLink.mockImplementationOnce(() => ({ _links: { self: { href: 'http://example.com/path/to/somewhere' } }, name: riverName }))
+      const mapped = await activitiesApi.doMap(({ }), createMockContext({ href: 'http://example.com/path/to/somewhere', riverName }))
+      expect(mapped.river.name).toEqual(expectedValue)
+    })
+
+    it.each([
+      ['Tyne', 'Tyne'],
+      ['Barry', 'Barry'],
+      ['Usk', 'Usk']
+    ])('sets river internal value', async (riverInternal, expectedValue) => {
+      const activitiesApi = new ActivitiesApi()
+      mockGetFromLink.mockImplementationOnce(() => ({ _links: { self: { href: 'http://example.com/path/to/somewhere' } }, internal: riverInternal }))
+      const mapped = await activitiesApi.doMap(({ }), createMockContext({ href: 'http://example.com/path/to/somewhere', riverInternal }))
+      expect(mapped.river.internal).toEqual(expectedValue)
+    })
+
+    it.each([
+      ['http://example.com/path/to/somewhere', '/path', 'Tyne', 'Tyne', 'internal', 'internal'],
+      ['http://example.com/unwise/path/to/the/dark/side', 'the', 'Tyne', 'Tyne', 'internal', 'internal'],
+      ['http://example.com/unwise/path/to/the/dark/side/', 'side', 'Tyne', 'Tyne', 'internal', 'internal']
+    ])('sets the _links attribute values id, name and internal', async (href, apiPath, name, nameExpectedValue, internal, internalExpectedValue) => {
+      process.env.API_PATH = apiPath
+      const activitiesApi = new ActivitiesApi()
+      mockGetFromLink.mockImplementationOnce(() => ({ _links: { self: { href: href } } }))
+      const mapped = await activitiesApi.doMap(({ }), createMockContext({ href, name, internal }))
+      expect(mapped._links.river.href).toEqual(href)
+      expect(mapped._links.river.name).toEqual(nameExpectedValue)
+      expect(mapped._links.river.internal).toEqual(internalExpectedValue)
+      expect(mapped._links.self.href).toEqual(href)
     })
   })
 
-  describe.skip('add', () => {
+  describe('add', () => {
     it('should display the authentication page', async () => {
       const activitiesApi = new ActivitiesApi()
-
-      const request = {}
+      const request = {
+        cache: () => ({
+          get: jest.fn(() => ({ authorization: null }))
+        })
+      }
+      const href = 'http://example.com/path/to/somewhere'
       const submissionId = '1'
       const river = 'Goyt'
       const daysFishedWithMandatoryRelease = 1
       const daysFishedOther = 1
+      mockRequest.mockImplementationOnce(() => ({ id: '1', _links: { self: { href: href } }, river, daysFishedOther, daysFishedWithMandatoryRelease }))
+      const result = await activitiesApi.add(request, { submissionId, river, daysFishedWithMandatoryRelease, daysFishedOther })
+      expect(result.id).toEqual('/path/to/somewhere')
+      expect(result.daysFishedOther).toEqual(daysFishedOther)
+      expect(result.daysFishedWithMandatoryRelease).toEqual(daysFishedWithMandatoryRelease)
+      expect(result.river).toEqual(river)
+    })
 
-      await activitiesApi.add(request, submissionId, river, daysFishedWithMandatoryRelease, daysFishedOther)
-      expect(EntityApi.prototype.add).toBeCalledWith(request, {
-        submission: submissionId, river, daysFishedWithMandatoryRelease, daysFishedOther
-      })
+    it('should display error if there is an error', async () => {
+      const activitiesApi = new ActivitiesApi()
+      const request = {
+        cache: () => ({
+          get: jest.fn(() => ({ authorization: null }))
+        })
+      }
+      const errors = 'errors'
+      const href = 'http://example.com/path/to/somewhere'
+      const submissionId = '1'
+      const river = 'Goyt'
+      const daysFishedWithMandatoryRelease = 1
+      const daysFishedOther = 1
+      mockRequest.mockImplementationOnce(() => ({ id: 1, errors, _links: { self: { href: href } }, river, daysFishedOther, daysFishedWithMandatoryRelease }))
+      const result = await activitiesApi.add(request, { submissionId, river, daysFishedWithMandatoryRelease, daysFishedOther })
+      expect(result.id).toEqual(1)
     })
   })
 
-  describe.skip('change', () => {
+  describe('change', () => {
     it('should change the days', async () => {
       const activitiesApi = new ActivitiesApi()
-      const request = {}
+      const request = {
+        cache: () => ({
+          get: jest.fn(() => ({ authorization: null }))
+        })
+      }
+      const href = 'http://example.com/path/to/somewhere'
       const activityId = '1'
       const submissionId = '1'
       const riverId = '1'
       const daysFishedWithMandatoryRelease = 1
       const daysFishedOther = 1
+      mockRequest.mockImplementationOnce(() => ({ id: '1', _links: { self: { href: href } }, activityId, riverId, daysFishedOther, daysFishedWithMandatoryRelease }))
+      const result = await activitiesApi.change(request, { submissionId, activityId, riverId, daysFishedWithMandatoryRelease, daysFishedOther })
+      expect(result.id).toEqual('/path/to/somewhere')
+      expect(result.activityId).toEqual(activityId)
+      expect(result.daysFishedOther).toEqual(daysFishedOther)
+      expect(result.daysFishedWithMandatoryRelease).toEqual(daysFishedWithMandatoryRelease)
+      expect(result.riverId).toEqual(riverId)
+    })
 
-      await activitiesApi.change(request, activityId, submissionId, riverId, daysFishedWithMandatoryRelease, daysFishedOther)
-      expect(EntityApi.prototype.change).toBeCalledWith(request, activityId, {
-        daysFishedWithMandatoryRelease: daysFishedWithMandatoryRelease,
-        daysFishedOther: daysFishedOther,
-        river: riverId
-      })
+    it('should display error if there is an error', async () => {
+      const activitiesApi = new ActivitiesApi()
+      const request = {
+        cache: () => ({
+          get: jest.fn(() => ({ authorization: null }))
+        })
+      }
+      const errors = 'errors'
+      const href = 'http://example.com/path/to/somewhere'
+      const activityId = '1'
+      const submissionId = '1'
+      const riverId = '1'
+      const daysFishedWithMandatoryRelease = 1
+      const daysFishedOther = 1
+      mockRequest.mockImplementationOnce(() => ({ id: 1, errors, _links: { self: { href: href } }, activityId, riverId, daysFishedOther, daysFishedWithMandatoryRelease }))
+      const result = await activitiesApi.change(request, { submissionId, activityId, riverId, daysFishedWithMandatoryRelease, daysFishedOther })
+      expect(result.id).toEqual(1)
     })
   })
 
-  describe.skip('sort', () => {
+  describe('sort', () => {
     it('if in alphabetical order should not change', async () => {
       const activitiesApi = new ActivitiesApi()
       const a = {
@@ -125,40 +252,33 @@ describe('activities', () => {
 })
 
 const createMockContext = params => {
-  const { href, daysFishedOther, daysFishedWithMandatoryRelease } = {
-    href: '/path/to/somewhere',
+  const { href, daysFishedOther, daysFishedWithMandatoryRelease, name, riverName, internal } = {
+    href: 'http://example.com/path/to/somewhere',
     daysFishedOther: 0,
     daysFishedWithMandatoryRelease: 0,
+    name: 'river',
+    riverName: 'a river',
+    internal: 'internal',
     ...params
   }
   return {
-    _links: {
-      self: {
-        href
-      }
-    },
+    href,
     daysFishedOther,
-    daysFishedWithMandatoryRelease
-  }
-}
-
-const createMockRiver = params => {
-  const { href, daysFishedOther, daysFishedWithMandatoryRelease } = {
-    href: '/path/to/somewhere',
-    daysFishedOther: 0,
-    daysFishedWithMandatoryRelease: 0,
-    ...params
-  }
-  return {
+    daysFishedWithMandatoryRelease,
+    river: {
+      href,
+      riverName,
+      internal
+    },
     _links: {
       river: {
-        href
+        href,
+        name,
+        internal
       },
       self: {
         href
       }
-    },
-    daysFishedOther,
-    daysFishedWithMandatoryRelease
+    }
   }
 }
