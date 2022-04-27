e8f9d71df5115531ac473ff918db5b2be0e2692d
ScottDormand96 <sdormand@hotmail.com>
2022-04-27 09:25:04 +0100

    updated tests

--- test-jest/api/age-weight-key.spec.js
+++ test-jest/api/age-weight-key.spec.js
@@ -1,6 +1,7 @@
 const mockRequest = jest.fn()
 const mockRequestFileUpload = jest.fn()
 const AgeWeightKeyApi = require('../../src/api/age-weight-key')
+const client = require('../../src/api/client')
 
 jest.mock('../../src/api/client', () => {
   const originalModule = jest.requireActual('../../src/api/client')
@@ -24,6 +25,16 @@ describe('age-weight-key', () => {
       const result = await AgeWeightKeyApi.getByYear({}, year)
       expect(result).toStrictEqual({ rows: ['1', '2', '3', '4'] })
     })
+
+    it.each([
+      ['1996'],
+      ['1990'],
+      ['2021']
+    ])('return Client.request with relevant arguments', async (year) => {
+      mockRequest.mockImplementationOnce(() => ({ rows: [null, client.method.GET, 'reporting/reference/grilse-probabilities/' + year] }))
+      const result = await AgeWeightKeyApi.getByYear({}, year)
+      expect(result).toStrictEqual({ rows: [null, client.method.GET, 'reporting/reference/grilse-probabilities/' + year] })
+    })
   })
 
   describe('postNew', () => {
@@ -36,5 +47,15 @@ describe('age-weight-key', () => {
       const result = await AgeWeightKeyApi.postNew({}, year, gate, filePath, overwrite)
       expect(result).toStrictEqual({ rows: ['1', '2', '3', '4'] })
     })
+  })
+
+  it.each([
+    ['1996', 'gate', 'file path', true],
+    ['1990', 'gates', 'another file path', false],
+    ['2021', 'another gate', 'a different file path', true]
+  ])('returns Client.requestFileUpload with relevant arguments', async (year, gate, filePath, overwrite) => {
+    mockRequestFileUpload.mockImplementationOnce(() => ({ rows: [null, 'reporting/reference/grilse-probabilities/' + year + gate, overwrite, filePath] }))
+    const result = await AgeWeightKeyApi.postNew({}, year, gate, filePath, overwrite)
+    expect(result).toStrictEqual({ rows: [null, 'reporting/reference/grilse-probabilities/' + year + gate, overwrite, filePath] })
   })
 })
