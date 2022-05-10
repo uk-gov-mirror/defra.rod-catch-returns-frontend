742e866b591f296683975326c0e4a2e8cf64905c
ScottDormand96 <sdormand@hotmail.com>
2022-05-10 14:44:56 +0100

    added tests following feedback

--- test-jest/api/age-weight-key.spec.js
+++ test-jest/api/age-weight-key.spec.js
@@ -1,61 +1,116 @@
-const mockRequest = jest.fn()
-const mockRequestFileUpload = jest.fn()
 const AgeWeightKeyApi = require('../../src/api/age-weight-key')
-const client = require('../../src/api/client')
+const Client = require('../../src/api/client')
 
 jest.mock('../../src/api/client', () => {
   const originalModule = jest.requireActual('../../src/api/client')
   return {
     ...originalModule,
-    request: mockRequest,
-    requestFileUpload: mockRequestFileUpload
+    request: jest.fn(),
+    requestFileUpload: jest.fn()
   }
 })
 
 describe('age-weight-key', () => {
   beforeEach(jest.clearAllMocks)
 
-  describe('getByYear', () => {
-    it.each([
-      ['1996'],
-      ['1990'],
-      ['2021']
-    ])('return Client.request', async (year) => {
-      mockRequest.mockImplementationOnce(() => ({ rows: ['1', '2', '3', '4'] }))
-      const result = await AgeWeightKeyApi.getByYear({}, year)
-      expect(result).toStrictEqual({ rows: ['1', '2', '3', '4'] })
-    })
-
-    it.each([
-      ['1996'],
-      ['1990'],
-      ['2021']
-    ])('return Client.request with relevant arguments', async (year) => {
-      mockRequest.mockImplementationOnce(() => ({ rows: [null, client.method.GET, 'reporting/reference/grilse-probabilities/' + year] }))
-      const result = await AgeWeightKeyApi.getByYear({}, year)
-      expect(result).toStrictEqual({ rows: [null, client.method.GET, 'reporting/reference/grilse-probabilities/' + year] })
-    })
-  })
-
   describe('postNew', () => {
+    it('calls requestFileUpload with no authentication', async () => {
+      await AgeWeightKeyApi.postNew({}, '1999', 'iron-gate', '/path/to/a/file', true)
+      expect(Client.requestFileUpload).toHaveBeenCalledWith(
+        null,
+        expect.any(String),
+        expect.any(String),
+        expect.any(String)
+      )
+    })
+
     it.each([
-      ['1996', 'gate', 'file path', true],
-      ['1990', 'gates', 'another file path', false],
-      ['2021', 'another gate', 'a different file path', true]
-    ])('returns Client.requestFileUpload', async (year, gate, filePath, overwrite) => {
-      mockRequestFileUpload.mockImplementationOnce(() => ({ rows: ['1', '2', '3', '4'] }))
-      const result = await AgeWeightKeyApi.postNew({}, year, gate, filePath, overwrite)
-      expect(result).toStrictEqual({ rows: ['1', '2', '3', '4'] })
+      ['1996', 'iron-gate'],
+      ['2017', 'wooden-gate'],
+      ['1999', 'straw-door']
+    ])('presents path in correct format', async (year, gate) => {
+      await AgeWeightKeyApi.postNew({}, year, gate, '/path/to/a/file', true)
+      expect(Client.requestFileUpload).toHaveBeenCalledWith(
+        null,
+        expect.stringMatching(`reporting/reference/grilse-probabilities/${year}/${gate}`),
+        expect.any(String),
+        expect.any(String)
+      )
+    })
+
+    it.each([
+      [true, 'overwrite=true'],
+      [false, 'overwrite=false'],
+      ['example', 'overwrite=true']
+    ])('presents overwrite in correct format', async (overwrite, expected) => {
+      await AgeWeightKeyApi.postNew({}, '1999', 'iron-gate', '/path/to/a/file', overwrite)
+      expect(Client.requestFileUpload).toHaveBeenCalledWith(
+        null,
+        expect.any(String),
+        expect.stringMatching(expected),
+        expect.any(String)
+      )
+    })
+
+    it.each([
+      ['/path/to/a/file'],
+      ['/path/to/another/file'],
+      ['/path/to/a/test/file']
+    ])('presents file path in correct format', async (filePath) => {
+      await AgeWeightKeyApi.postNew({}, '1999', 'iron-gate', filePath, true)
+      expect(Client.requestFileUpload).toHaveBeenCalledWith(
+        null,
+        expect.any(String),
+        expect.any(String),
+        expect.stringMatching(filePath)
+      )
+    })
+
+    it('returns the value of Client.requestFileUpload', async () => {
+      const returnValue = Symbol('return value')
+      Client.requestFileUpload.mockReturnValueOnce(returnValue)
+      const ret = await AgeWeightKeyApi.postNew({}, '1999', 'iron-gate', '/path', true)
+      expect(ret).toEqual(returnValue)
     })
   })
 
-  it.each([
-    ['1996', 'gate', 'file path', true],
-    ['1990', 'gates', 'another file path', false],
-    ['2021', 'another gate', 'a different file path', true]
-  ])('returns Client.requestFileUpload with relevant arguments', async (year, gate, filePath, overwrite) => {
-    mockRequestFileUpload.mockImplementationOnce(() => ({ rows: [null, 'reporting/reference/grilse-probabilities/' + year + gate, overwrite, filePath] }))
-    const result = await AgeWeightKeyApi.postNew({}, year, gate, filePath, overwrite)
-    expect(result).toStrictEqual({ rows: [null, 'reporting/reference/grilse-probabilities/' + year + gate, overwrite, filePath] })
+  describe('getByYear', () => {
+    it('calls request with no authentication', async () => {
+      await AgeWeightKeyApi.getByYear({}, '1999')
+      expect(Client.request).toHaveBeenCalledWith(
+        null,
+        expect.any(String),
+        expect.any(String)
+      )
+    })
+
+    it('presents client.method.GET in cirrect format', async () => {
+      await AgeWeightKeyApi.getByYear({}, '1999')
+      expect(Client.request).toHaveBeenCalledWith(
+        null,
+        expect.stringMatching('GET'),
+        expect.any(String)
+      )
+    })
+
+    it.each([
+      ['1996'],
+      ['2017'],
+      ['1999']
+    ])('presents path in correct format', async (year) => {
+      await AgeWeightKeyApi.getByYear({}, year)
+      expect(Client.request).toHaveBeenCalledWith(
+        null,
+        expect.any(String),
+        expect.stringMatching(`reporting/reference/grilse-probabilities/${year}`)
+      )
+    })
+
+    it('returns the value of Client.request', async () => {
+      const returnValue = Symbol('return value')
+      Client.request.mockReturnValueOnce(returnValue)
+      const ret = await AgeWeightKeyApi.getByYear({}, '1999')
+      expect(ret).toEqual(returnValue)
+    })
   })
 })
