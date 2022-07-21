5fbd298b59a77082fb7f84991e1d7a6f3d0e1234
Phil Benson <phil.benson@defra.gov.uk>
2022-07-21 12:32:34 +0100

    Decorate drink list as autocomplete

--- src/assets/javascript/rivers-autocomplete.js
+++ src/assets/javascript/rivers-autocomplete.js
@@ -3,14 +3,19 @@ const rivers = []
 for (let i = 0; i < riversSelect.length; i++) {
   rivers.push(riversSelect.options[i].text)
 }
+const drinksSelect = document.querySelector('#drink')
+const drinks = []
+for (let j = 0; j < drinksSelect.length; j++) {
+  drinks.push(drinksSelect.options[j].text)
+}
 
-function normalise(string) {
+function normalise (string) {
   const bracketIndex = string.indexOf('(')
   const stringWithoutBracketEnding = bracketIndex >= 0 ? string.slice(0, bracketIndex) : string
   return stringWithoutBracketEnding.toLowerCase().trim()
 }
 
-function score(name, query) {
+function score (name, query) {
   if (name.startsWith(query)) {
     return -2
   }
@@ -20,7 +25,7 @@ function score(name, query) {
   return 0
 }
 
-function getRiversSuggestions(query, populateResults) {
+function getRiversSuggestions (query, populateResults) {
   const filteredRivers = rivers.map(river => ({ name: river, score: score(normalise(river), normalise(query)) }))
     .filter(river => river.score < 0)
     .sort((a, b) => a.score - b.score)
@@ -29,6 +34,16 @@ function getRiversSuggestions(query, populateResults) {
   return populateResults(filteredRivers)
 }
 
+function getDrinkSuggestions (query, populateResults) {
+  const filteredDrinks = drinks.map(drink => ({ name: drink, score: score(normalise(drink), normalise(query)) }))
+    .filter(drink => drink.score < 0)
+    .sort((a, b) => a.score - b.score)
+    .map(drink => drink.name)
+
+  return populateResults(filteredDrinks)
+}
+
 module.exports = {
-  getRiversSuggestions
+  getRiversSuggestions,
+  getDrinkSuggestions
 }
