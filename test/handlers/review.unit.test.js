a73d1c8a8a678bb54ed7a3b6f331f99ca8c27205
Lawrence-Sithole-Def <lawrence.sithole@defra.gov.uk>
2026-09-10 09:32:03 +0100

    add test coverage for confirmation checkbox validation

--- test/handlers/review.unit.test.js
+++ test/handlers/review.unit.test.js
@@ -90,6 +90,28 @@ describe('review-handler.unit', () => {
       expect(h.redirect).toHaveBeenCalledWith('/confirmation')
     })
 
+    it('should redisplay the page when checkbox is not selected', async () => {
+      const cacheObj = { submissionId: 'submissions/1', year:2025, licenceNumber: 'AB1CD2', postcode: 'DC32 1BA', locked: false }
+      const request = getMockRequest(cacheObj, { continue: true})
+      const h = getMockH()
+
+      mockGetById.mockResolvedValueOnce({
+        reportingExclude: true
+      })
+
+      mockDisplayData.mockResolvedValueOnce({
+        activities: [],
+        catches: [],
+        smallCatches: [],
+        foundInternal: false
+      })
+      const handler = new ReviewHandler('review')
+
+      await handler.doPost(request, h)
+
+      expect(h.view).toHaveBeenCalledWith('review', expect.objectContaining({errors: true}))
+    })
+
     it('should unlock when unlock is present and CONTEXT=FMT', async () => {
       process.env.CONTEXT = 'FMT'
       const cacheObj = { submissionId: 'submissions/1', locked: true }
