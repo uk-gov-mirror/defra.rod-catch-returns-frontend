75895891653954d183703760ba2d0326e6d8b735
Lawrence-Sithole-Def <lawrence.sithole@defra.gov.uk>
2026-09-11 10:30:34 +0100

    fix code style issues

--- test/handlers/review.unit.test.js
+++ test/handlers/review.unit.test.js
@@ -70,7 +70,7 @@ describe('review-handler.unit', () => {
   describe('doPost', () => {
     it('should lock when continue is present', async () => {
       const cacheObj = { submissionId: 'submissions/1', locked: false }
-      const request = getMockRequest(cacheObj, { continue: true, confirm: 'yes'})
+      const request = getMockRequest(cacheObj, { continue: true, confirm: 'yes' })
       const h = getMockH()
       const handler = new ReviewHandler('review')
 
@@ -81,7 +81,7 @@ describe('review-handler.unit', () => {
 
     it('should redirect to confirmation when continue is present', async () => {
       const cacheObj = { submissionId: 'submissions/1', locked: false }
-      const request = getMockRequest(cacheObj, { continue: true, confirm: 'yes'})
+      const request = getMockRequest(cacheObj, { continue: true, confirm: 'yes' })
       const h = getMockH()
       const handler = new ReviewHandler('review')
 
@@ -91,8 +91,8 @@ describe('review-handler.unit', () => {
     })
 
     it('should redisplay the page when checkbox is not selected', async () => {
-      const cacheObj = { submissionId: 'submissions/1', year:2025, licenceNumber: 'AB1CD2', postcode: 'DC32 1BA', locked: false }
-      const request = getMockRequest(cacheObj, { continue: true})
+      const cacheObj = { submissionId: 'submissions/1', year: 2025, licenceNumber: 'AB1CD2', postcode: 'DC32 1BA', locked: false }
+      const request = getMockRequest(cacheObj, { continue: true })
       const h = getMockH()
 
       mockGetById.mockResolvedValueOnce({
@@ -109,7 +109,7 @@ describe('review-handler.unit', () => {
 
       await handler.doPost(request, h)
 
-      expect(h.view).toHaveBeenCalledWith('review', expect.objectContaining({errors: true}))
+      expect(h.view).toHaveBeenCalledWith('review', expect.objectContaining({ errors: true }))
     })
 
     it('should unlock when unlock is present and CONTEXT=FMT', async () => {
